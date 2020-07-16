ca35eb52833c960e05e87b2e1090c7daffc6397e
Zsolt Medgyesi <zmedgyes@gmail.com>
2020-07-16 19:18:11 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,53 @@
 #include "RFIDDevice.h"
 #include "RFIDMessage.h"
 
+#include "owasys.h"
+
 using namespace std;
 
 int main()
 {
-    SerialDevice *serial = new SerialDevice("/dev/USB0", B115200, 0);
-    RFIDDevice *rfid = new RFIDDevice(serial);
-    RFIDMessage *msgIn;
+    SerialDevice serial("/dev/ttyO1", B115200, 0);
+    RFIDDevice rfid(&serial);
 
-    unsigned char Source_0[9] = {0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x5F, 0x30, 0x00};
+    unsigned char Source_0[9] = {0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x5F, 0x30, 0x00}; //Source_0\0
     unsigned int protocol     = 0x00000003;
 
     unsigned int powerInitial = 199;
     unsigned int powerMiddle  = 80;
     unsigned int powerTesting = 19;
 
-    double gain = 8.0;
+    /*double gain = 8.0;
     double loss = 1.5;
-    double ERPPower = 2000.0;
+    double ERPPower = 2000.0;*/
 
     // Set Protocol
-    msgIn = rfid->setProtocol(protocol);
-    msgIn->print();
+    printf("Set Protocol\n");
+    rfid.setProtocol(protocol)->print();
+    printf("SET PROTOCOL COMPLETE\n");
+    getc(stdin);
+
+    // Get Power
+    printf("Get Power\n");
+    rfid.getPower()->print();
+    printf("GET POWER COMPLETE\n");
+    getc(stdin);
+
+    // Set Power
+    printf("Set Power\n");
+    rfid.setPower(powerMiddle)->print();
     printf("SET POWER COMPLETE\n");
     getc(stdin);
 
     // Get Power
     printf("Get Power\n");
-    msgIn = rfid->getPower();
-    msgIn->print();
-    printf("GET POWER COMPLETE\n");
-    getc(stdin);
-
-    // Set Power
-    msgIn = rfid->setPower(powerMiddle);
-    msgIn->print();
-    printf("SET POWER COMPLETE\n");
-    getc(stdin);
-
-    // Get Power
-    msgIn = rfid->getPower();
-    msgIn->print();
+    rfid.getPower()->print();
     printf("GET POWER COMPLETE\n");
     getc(stdin);
 
     // antenna status
-    msgIn = rfid->getAntennaStatus(Source_0, 9);
-    msgIn->print();
+    printf("Get Antenna Status\n");
+    rfid.getAntennaStatus(Source_0, 9)->print();
     printf("GET ANTENNA STATUS COMPLETE\n");
     getc(stdin);
 
@@ -58,20 +58,23 @@ int main()
 
     // Inventory
     printf("\n");
-
+    vector<unique_ptr<RFID>> founds;
     for(;;) {
-        msgIn = rfid->inventory(Source_0,9);
+        founds.clear();
+        printf("Inventory\n");
 
         int k=0;
 
-        vector<RFID> *founds = msgIn->getRFIDs();
+        rfid.inventory(Source_0, 9, nullptr, 0,0,0x0011)->getRFIDs(&founds); //with RSSI and TID
 
-        for(vector<RFID>::iterator it = founds->begin(); it != founds->end(); ++it){
-            printf("%s \n", RFIDDevice::parseRFID(*it));
+        for (auto &p_el : founds)
+        {
+            printf("%s \n", RFIDDevice::parseRFID(p_el.get()));
             k++;
         }
         printf("\n");
-        sleep(1);
+
+        sleep(5);
     }
 
     printf("\n");
